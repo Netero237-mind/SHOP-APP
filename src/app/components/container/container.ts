c887b8b963991b1cc7ce48215334b4cd937b0bdc
WILSON <wilsonnetero@gmail.com>
2026-06-26 13:13:47 +0100

    fix: merge remote changes and keep local components

--- src/app/components/container/container.ts
+++ src/app/components/container/container.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { JsonPipe } from '@angular/common';
 import { Productlist } from "../productlist/productlist";
 
 @Component({
   selector: 'app-container',
-  imports: [Productlist],
+  imports: [Productlist, JsonPipe],
   templateUrl: './container.html',
   styleUrl: './container.css',
 })
-export class Container {}
+export class Container {
+  product = signal({ 
+  name: 'Robe pour femme', 
+  price: 12.99 
+}); 
+}
